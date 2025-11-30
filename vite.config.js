94ff269c2773e8ab12cb5553ce2b6a84175dd19b
Ashwini Gaikwad <ashugaikwad1997@gmail.com>
2025-11-30 20:31:38 +0530

    my  new  updated build

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    /* base: '/' */   // ← Add this line
- plugins: [react()],
-  base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/websiteapp/' : '/'
+    base: '/websiteapp/'   // ← Add this line
 
 })
 /* https://arshada1997.github.io/websiteapp/ */
