fed4f940c940111a8ccb763e5989c20f0e04c91b
Ashwini Gaikwad <ashugaikwad1997@gmail.com>
2025-11-30 20:08:15 +0530

    updated build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/websiteapp/'   // ← Add this line
+    base: '/'   // ← Add this line
 
 })
 /* https://arshada1997.github.io/websiteapp/ */
